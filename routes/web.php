94109969c2ea8b392b9391a3ac50dd014454ed10
Mohammed Abed <mjebrini@jebrini-tech.com>
2020-04-11 17:02:26 +0300

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+Route::get('/', function () { return redirect('/admin/home'); });
+
+Auth::routes(['register' => false]);
+
+// Change Password Routes...
+Route::get('change_password', 'Auth\ChangePasswordController@showChangePasswordForm')->name('auth.change_password');
+Route::patch('change_password', 'Auth\ChangePasswordController@changePassword')->name('auth.change_password');
+
+Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::post('abilities/destroy', 'AbilitiesController@massDestroy')->name('abilities.massDestroy');
+    Route::resource('abilities', 'AbilitiesController');
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+});
