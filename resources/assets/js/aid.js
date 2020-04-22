82c7a0dd501fc8eebfd495bb60978c89bd1a6e5e
Mohammed Abed <mjebrini@jebrini-tech.com>
2020-04-22 17:54:00 +0300

    adding vue views with fb login

---
+++ resources/assets/js/aid.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'; 
+import VueRouter from 'vue-router';
+import * as auth from './facebook';
+
+window.Vue = Vue; 
+window.VueRouter = VueRouter;
+
+Vue.use(VueRouter)
+
+import Home from './components/Home.vue';
+
+// routers 
+const routes = [
+    { path: '/', component:Home},
+    { path : '/activities', component :  require('./components/activities.vue')}
+];
+
+const router = new VueRouter({ 
+    base : 'covid19',
+    mode: 'history',
+    routes : routes });
+
+// shared compoenents 
+Vue.component('navbar-component', require('./components/Navbar.vue') );
+
+
+// shared config 
+Vue.config.ignoredElements = ['fb:login-button'];
+
+
+
+window.onAppReady = function() { 
+
+    window.store = {user:null};
+    auth.default.login(function(user){
+        window.store.user = user;
+        console.info("User logged in ", user);
+        $('body').addClass('loggedin');
+    }, function(err){ 
+        console.info("User isn't logged in ", err);
+        $('body').addClass('notloggedin');
+    });
+
+    new Vue({
+        router 
+    }).$mount('#aid-app');
+    
+    
+}
---
+++ resources/views/layouts/aid.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ trans('panel.site_title') }}</title> 
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    @yield('styles')
+</head>
+
+<body class="aid-page">
+   
+    @yield("content")
+      
+    @yield('scripts')
+    <script async src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
